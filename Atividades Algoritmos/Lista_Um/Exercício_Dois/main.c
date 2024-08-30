502af926eb7d2c754bfc37cb1114f29f209961b0
Erica-Kne <erica-knevitz@outlook.com>
2024-08-30 18:11:31 -0300

    entrega de alguns exercícios
    Entrega de alguns exercícios que eu fiz.

---
+++ Atividades Algoritmos/Lista_Um/Exercício_Dois/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    char nome [50] = " ";
+
+    printf("Informe o seu nome:\n");
+    scanf("%s", nome);
+    fflush(stdin);
+
+    printf("Boas-vindas, %s!\n", nome);
+    
+    return 0;
+}
---
+++ Atividades Algoritmos/Lista_Um/Exercício_Tres/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    char respostaCorreta [2] = "A";
+    char respostaDoUsuario [2] = " ";
+
+    printf("Qual e o verdadeiro nome do super-heroi Batman?\nA)Bruce Wayne\nB)Clark Kent\nC)Peter Parker\nD)Tony Stark\nE)Steve Rogers\n");
+    
+    scanf("%s", respostaDoUsuario);
+    fflush(stdin);
+
+    printf("Voce respondeu a alternativa %s. A resposta correta e a alternativa %s\n", respostaDoUsuario, respostaCorreta);
+    
+    return 0;
+}
